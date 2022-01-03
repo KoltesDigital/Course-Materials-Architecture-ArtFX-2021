KoltesDigital/Game-Architecture-Epita-2018

Remove singletons + Add EventListener + Add EntityContext

StealthFactor/code/engine/gameplay/entities/Target.cpp
@@ -1,5 +1,6 @@
 #include "Target.hpp"
 
+#include <engine/gameplay/EntityContext.hpp>
 #include <engine/gameplay/GameplayManager.hpp>
 #include <engine/graphics/GraphicsManager.hpp>
 #include <engine/physics/PhysicsManager.hpp>
@@ -10,11 +11,12 @@ namespace engine
 	{
 		namespace entities
 		{
-			Target::Target()
+			Target::Target(EntityContext &context)
+				: Entity{ context }
 			{
 				shapeList.load("target");
 
-				collisionGeomId = dCreateBox(physics::Manager::getInstance().getSpaceId(), gameplay::Manager::CELL_SIZE * 0.9f, gameplay::Manager::CELL_SIZE * 0.9f, 1.f);
+				collisionGeomId = dCreateBox(context.physicsManager.getSpaceId(), gameplay::Manager::CELL_SIZE * 0.9f, gameplay::Manager::CELL_SIZE * 0.9f, 1.f);
 				dGeomSetData(collisionGeomId, this);
 			}
 
@@ -31,7 +33,7 @@ namespace engine
 
 			void Target::draw()
 			{
-				graphics::Manager::getInstance().draw(shapeList, getTransform());
+				context.graphicsManager.draw(shapeList, getTransform());
 			}
 		}
 	}

StealthFactor/code/engine/physics/PhysicsManager.cpp
@@ -6,24 +6,27 @@ namespace engine
 {
 	namespace physics
 	{
-		Manager *Manager::instance = nullptr;
-
 		Manager::Collision::Collision(dGeomID o1, dGeomID o2)
 			: o1{ o1 }
 			, o2{ o2 }
 		{
 		}
 
-		Manager::Manager()
+		bool Manager::setUp()
 		{
 			dInitODE();
 
 			spaceId = dHashSpaceCreate(0);
+			return spaceId != nullptr;
 		}
 
-		Manager::~Manager()
+		void Manager::tearDown()
 		{
-			dSpaceDestroy(spaceId);
+			if (spaceId != nullptr)
+			{
+				dSpaceDestroy(spaceId);
+			}
+
 			dCloseODE();
 		}
 
@@ -61,14 +64,6 @@ namespace engine
 		{
 			auto &frameCollisions = *reinterpret_cast<Collisions *>(data);
 			frameCollisions.emplace_back(o1, o2);
-		}
-
-		Manager &Manager::getInstance()
-		{
-			if (!instance)
-				instance = new Manager();
-
-			return *instance;
 		}
 	}
 }

KoltesDigital/Course-Materials-Architecture-ArtFX-2021

StealthFactor/code/engine/gameplay/entities/Target.cpp
@@ -1,5 +1,6 @@
 #include "Target.hpp"
 
+#include <engine/gameplay/EntityContext.hpp>
 #include <engine/gameplay/GameplayManager.hpp>
 #include <engine/graphics/GraphicsManager.hpp>
 #include <engine/physics/PhysicsManager.hpp>
@@ -10,11 +11,12 @@ namespace engine
 	{
 		namespace entities
 		{
-			Target::Target()
+			Target::Target(EntityContext& context)
+				: Entity{ context }
 			{
 				_shapeList.load("target");
 
-				_collisionGeomId = dCreateBox(physics::Manager::getInstance().getSpaceId(), gameplay::Manager::CELL_SIZE * 0.9f, gameplay::Manager::CELL_SIZE * 0.9f, 1.f);
+				_collisionGeomId = dCreateBox(context.physicsManager.getSpaceId(), gameplay::Manager::CELL_SIZE * 0.9f, gameplay::Manager::CELL_SIZE * 0.9f, 1.f);
 				dGeomSetData(_collisionGeomId, this);
 			}
 
@@ -31,7 +33,7 @@ namespace engine
 
 			void Target::draw()
 			{
-				graphics::Manager::getInstance().draw(_shapeList, getTransform());
+				_context.graphicsManager.draw(_shapeList, getTransform());
 			}
 		}
 	}

StealthFactor/code/engine/physics/PhysicsManager.cpp
@@ -6,24 +6,27 @@ namespace engine
 {
 	namespace physics
 	{
-		Manager* Manager::instance = nullptr;
-
 		Manager::Collision::Collision(dGeomID o1, dGeomID o2)
 			: o1{ o1 }
 			, o2{ o2 }
 		{
 		}
 
-		Manager::Manager()
+		bool Manager::setUp()
 		{
 			dInitODE();
 
 			_spaceId = dHashSpaceCreate(0);
+			return _spaceId != nullptr;
 		}
 
-		Manager::~Manager()
+		void Manager::tearDown()
 		{
-			dSpaceDestroy(_spaceId);
+			if (_spaceId != nullptr)
+			{
+				dSpaceDestroy(_spaceId);
+			}
+
 			dCloseODE();
 		}
 
@@ -61,14 +64,6 @@ namespace engine
 		{
 			auto& frameCollisions = *reinterpret_cast<Collisions*>(data);
 			frameCollisions.emplace_back(o1, o2);
-		}
-
-		Manager& Manager::getInstance()
-		{
-			if (!instance)
-				instance = new Manager();
-
-			return *instance;
 		}
 	}
 }